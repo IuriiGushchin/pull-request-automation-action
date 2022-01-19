c9636251ba146a7709ed3e113716459f615d7d19
Yuriy Gushchin <iuriigushchin@yandex.ru>
2022-01-19 19:30:22 +0300

    in progress

--- index.js
+++ index.js
@@ -3,18 +3,11 @@ const github = require("@actions/github");
 
 async function run() {
   try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput("who-to-greet");
     const orgName = core.getInput("ORGANIZATION");
     const projectNumber = core.getInput("PROJECT_NUMBER");
+    const pullRequestId = core.getInput("PR_ID");
 
     const myToken = core.getInput("GITHUB_TOKEN");
-
-    // const nameToGreet = "Yuri";
-    // const orgName = "TestingOrganizationForAutomation";
-    // const projectNumber = "1";
-
-    // const myToken = "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";
     const octokit = github.getOctokit(myToken);
 
     const projectQuery = `
@@ -42,90 +35,102 @@ async function run() {
       org: orgName,
       number: parseInt(projectNumber),
     });
-    console.log(projectResult);
 
-    PROJECT_ID = projectResult.organization.projectNext.id;
-    STATUS_FIELD_ID = projectResult.organization.projectNext.fields.nodes.find(
-      (o) => o.name === "Status"
-    ).id;
-    TODO_OPTION_ID = JSON.parse(
+    const projectId = projectResult.organization.projectNext.id;
+    const statusFieldId =
+      projectResult.organization.projectNext.fields.nodes.find(
+        (o) => o.name === "Status"
+      ).id;
+    const pullRequestedOptionId = JSON.parse(
       projectResult.organization.projectNext.fields.nodes.find(
         (o) => o.name === "Status"
       ).settings
     ).options.find((o) => o.name === "In Progress").id;
-    PROJECT_CARDS = projectResult.organization.projectNext.items.nodes;
-    console.log(PROJECT_ID);
-    console.log(STATUS_FIELD_ID);
-    console.log(TODO_OPTION_ID);
-    console.log(PROJECT_CARDS);
+    const projectCards = projectResult.organization.projectNext.items.nodes;
 
-    console.log("PR");
-    const pullRequestId = core.getInput("PR_ID");
-    console.log(pullRequestId);
-    console.log("PR");
+    const issuesQuery = `
+    query($pr: ID!) {
+      node(id: $pr) {
+        ... on PullRequest {
+          timelineItems(itemTypes: [CONNECTED_EVENT, DISCONNECTED_EVENT], first: 100) {
+            nodes {
+              ... on ConnectedEvent {
+                id
+                subject {
+                  ... on Issue {
+                    number
+                    title
+                  }
+                }
+              }
+              ... on DisconnectedEvent {
+                id
+                subject {
+                  ... on Issue {
+                    title
+                    number
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`;
 
-    // const issuesQuery = `
-    // query($pr: ID!) {
-    //   node(id: $pr) {
-    //     ... on PullRequest {
-    //       timelineItems(itemTypes: [CONNECTED_EVENT, DISCONNECTED_EVENT], first: 100) {
-    //         nodes {
-    //           ... on ConnectedEvent {
-    //             id
-    //             subject {
-    //               ... on Issue {
-    //                 id
-    //                 number
-    //                 url
-    //                 title
-    //               }
-    //             }
-    //           }
-    //           ... on DisconnectedEvent {
-    //             id
-    //             subject {
-    //               ... on Issue {
-    //                 title
-    //                 number
-    //                 url
-    //               }
-    //             }
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // }`
+    const issuesResult = await octokit.graphql(issuesQuery, {
+      pr: pullRequestId,
+    });
 
-    // const issuesResult = await octokit.graphql(issuesQuery, {
-    //   org: orgName,
-    //   number: parseInt(projectNumber),
-    // });
+    const resource = issuesResult;
+    const issues = {};
+    resource.node.timelineItems.nodes.map((node) => {
+      if (issues.hasOwnProperty(node.subject.number)) {
+        issues[node.subject.number]++;
+      } else {
+        issues[node.subject.number] = 1;
+      }
+    });
+    const linkedIssues = [];
+    for (const [issue, count] of Object.entries(issues)) {
+      if (count % 2 !== 0) {
+        linkedIssues.push(parseInt(issue));
+      }
+    }
 
-    // const resource = issuesResult.data.data.resource
-    // const issues = {}
-    // resource.timelineItems.nodes.map((node) => {
-    //   if (issues.hasOwnProperty(node.subject.number)) {
-    //     issues[node.subject.number]++
-    //   } else {
-    //     issues[node.subject.number] = 1
-    //   }
-    // })
-    // const linkedIssues = []
-    // for (const [issue, count] of Object.entries(issues)) {
-    //   if (count % 2 !== 0) {
-    //     linkedIssues.push(issue)
-    //   }
-    // }
-    // console.log(issues)
+    const linkedIssuesTitles = resource.node.timelineItems.nodes
+      .filter((n) => linkedIssues.includes(n.subject.number))
+      .map((n) => n.subject.title);
 
-    // console.log(linkedIssues)
-    console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
-    // console.log(`The event payload: ${payload}`);
+    const projectCardIds = projectCards
+      .filter((c) => linkedIssuesTitles.includes(c.title))
+      .map((c) => c.id);
+    for (const projectCardId of projectCardIds) {
+      const moveCardQuery = `
+      mutation (
+        $project: ID!
+        $item: ID!
+        $status_field: ID!
+        $status_value: String!
+      ) {
+        set_status: updateProjectNextItemField(input: {
+          projectId: $project
+          itemId: $item
+          fieldId: $status_field
+          value: $status_value
+        }) {
+          projectNextItem {
+            id
+            }
+        }
+      }`;
+      const issuesResult = await octokit.graphql(moveCardQuery, {
+        project: projectId,
+        item: projectCardId,
+        status_field: statusFieldId,
+        status_value: pullRequestedOptionId,
+      });
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
