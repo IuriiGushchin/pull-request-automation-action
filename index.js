170436d7b8a10419107b4d1eb1525da28a0ba3d9
Yuriy Gushchin <iuriigushchin@yandex.ru>
2022-01-26 00:40:44 +0300

    labeling test

--- index.js
+++ index.js
@@ -3,50 +3,11 @@ const github = require("@actions/github");
 
 async function run() {
   try {
-    const orgName = core.getInput("ORGANIZATION");
-    const projectNumber = core.getInput("PROJECT_NUMBER");
     const pullRequestId = core.getInput("PR_ID");
+    const branchName = core.getInput("BRANCH_NAME");
 
     const myToken = core.getInput("GITHUB_TOKEN");
     const octokit = github.getOctokit(myToken);
-
-    const projectQuery = `
-    query($org: String!, $number: Int!) {
-      organization(login: $org){
-        projectNext(number: $number) {
-          id
-          fields(first:20) {
-            nodes {
-              id
-              name
-              settings
-            }
-          }
-          items(last: 100) {
-            nodes {
-              title
-              id
-            }
-          }
-        }
-      }
-    }`;
-    const projectResult = await octokit.graphql(projectQuery, {
-      org: orgName,
-      number: parseInt(projectNumber),
-    });
-
-    const projectId = projectResult.organization.projectNext.id;
-    const statusFieldId =
-      projectResult.organization.projectNext.fields.nodes.find(
-        (o) => o.name === "Status"
-      ).id;
-    const pullRequestedOptionId = JSON.parse(
-      projectResult.organization.projectNext.fields.nodes.find(
-        (o) => o.name === "Status"
-      ).settings
-    ).options.find((o) => o.name === "In Progress").id;
-    const projectCards = projectResult.organization.projectNext.items.nodes;
 
     const issuesQuery = `
     query($pr: ID!) {
@@ -74,6 +35,12 @@ async function run() {
               }
             }
           }
+          repository {
+            name,
+            owner {
+              login
+            }
+          }
         }
       }
     }`;
@@ -81,58 +48,46 @@ async function run() {
     const issuesResult = await octokit.graphql(issuesQuery, {
       pr: pullRequestId,
     });
+    const repoName = issuesResult.node.repository.name;
+    const ownerName = issuesResult.node.repository.owner.login;
 
-    const resource = issuesResult;
     const issues = {};
-    resource.node.timelineItems.nodes.map((node) => {
+    issuesResult.node.timelineItems.nodes.map((node) => {
       if (issues.hasOwnProperty(node.subject.number)) {
         issues[node.subject.number]++;
       } else {
         issues[node.subject.number] = 1;
       }
     });
-    const linkedIssues = [];
+    const linkedIssuesNumbers = [];
     for (const [issue, count] of Object.entries(issues)) {
       if (count % 2 !== 0) {
-        linkedIssues.push(parseInt(issue));
+        linkedIssuesNumbers.push(parseInt(issue));
       }
     }
 
-    const linkedIssuesTitles = resource.node.timelineItems.nodes
-      .filter((n) => linkedIssues.includes(n.subject.number))
-      .map((n) => n.subject.title);
-
-    const projectCardIds = projectCards
-      .filter((c) => linkedIssuesTitles.includes(c.title))
-      .map((c) => c.id);
-    for (const projectCardId of projectCardIds) {
-      const moveCardQuery = `
-      mutation (
-        $project: ID!
-        $item: ID!
-        $status_field: ID!
-        $status_value: String!
-      ) {
-        set_status: updateProjectNextItemField(input: {
-          projectId: $project
-          itemId: $item
-          fieldId: $status_field
-          value: $status_value
-        }) {
-          projectNextItem {
-            id
-            }
-        }
-      }`;
-      const issuesResult = await octokit.graphql(moveCardQuery, {
-        project: projectId,
-        item: projectCardId,
-        status_field: statusFieldId,
-        status_value: pullRequestedOptionId,
+    for (let linkedIssueNumber of linkedIssuesNumbers) {
+      var updatedIssueInformation = await octokit.rest.issues.get({
+        owner: ownerName,
+        repo: repoName,
+        issue_number: linkedIssueNumber,
       });
+      let labels = updatedIssueInformation.data.labels.map(
+        (label) => label.name
+      );
+      labels.push(branchName === "dev" ? "InDev" : "InTest");
+
+      const result = await octokit.rest.issues.update({
+        owner: ownerName,
+        repo: repoName,
+        issue_number: linkedIssueNumber,
+        labels: labels,
+      });
+      console.log("Edited issue data --- ", result);
     }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
+
 run();
